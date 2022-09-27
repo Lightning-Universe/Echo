5dfab4b0a08e547fd358a03488abaf06437e9713
Alec Merdler <alecmerdler@gmail.com>
2022-09-27 13:49:58 -0700

    added scaffolding files

---
+++ echo/__about__.py
@@ -0,0 +1,20 @@
+__version__ = "0.0.1"
+__author__ = "Lightning et al."
+__author_email__ = "alec@lightning.ai"
+__license__ = "MIT"
+__copyright__ = f"Copyright (c) 2021-2022, {__author__}."
+__homepage__ = "https://github.com/Lightning-AI/LAI-Echo-App"
+__docs__ = "Speech recognition with OpenAI Whisper"
+__long_doc__ = """
+Speech recognition with OpenAI Whisper.
+"""
+
+__all__ = [
+    "__author__",
+    "__author_email__",
+    "__copyright__",
+    "__docs__",
+    "__homepage__",
+    "__license__",
+    "__version__",
+]
