a70f129c1dde160b139b3a48e7246b1344e07ebb
Alec Merdler <alecmerdler@gmail.com>
2022-11-04 13:04:20 +0000

    Use FileServer Instead of Drive in YouTuber Component
    Deleting Echoes was not working because Lightning Drives only
    allow the component which added a file to delete it. When
    creating an Echo from a YouTube URL, the YouTuber component
    was adding the file directly into the Drive, which prevented
    the FileServer component from deleting it.
    
    This changes the YouTuber component to instead upload the file
    to the FileServer component instead of putting it directly into
    the Drive.

--- echo/components/youtuber.py
+++ echo/components/youtuber.py
@@ -1,8 +1,9 @@
-import os
+import pathlib
+import tempfile
 from dataclasses import dataclass
 
+import requests
 from lightning import BuildConfig, CloudCompute, LightningWork
-from lightning.app.storage import Drive
 from lightning.app.utilities.app_helpers import Logger
 from pytube import YouTube
 
@@ -26,42 +27,41 @@ class CustomBuildConfig(BuildConfig):
 class YouTuber(LightningWork):
     """Handles downloading and extracting videos from YouTube."""
 
-    def __init__(self, drive: Drive, cloud_compute=DEFAULT_CLOUD_COMPUTE, base_dir: str = None):
+    def __init__(self, cloud_compute=DEFAULT_CLOUD_COMPUTE, base_dir: str = None):
         super().__init__(
             parallel=True,
-            cloud_compute=CloudCompute(DEFAULT_CLOUD_COMPUTE),
+            cloud_compute=CloudCompute(cloud_compute),
             cloud_build_config=CustomBuildConfig(requirements=["pytube"]),
         )
 
         init_sentry()
 
-        self.drive = drive
         self.base_dir = base_dir
 
-    def run(self, youtube_url: str, echo_id: str):
+    def run(self, youtube_url: str, echo_id: str, fileserver_url: str):
         """Download a YouTube video and save it to the shared Drive."""
+        # NOTE: Dummy Echo is used to spin up the cloud machine on app startup so subsequent requests are faster
+        if youtube_url == DUMMY_YOUTUBE_URL and echo_id == DUMMY_ECHO_ID:
+            logger.info("Skipping dummy Echo")
+            return
+
         if not youtube_url:
             raise ValueError("No YouTube URL provided.")
 
         if not echo_id:
             raise ValueError("No Echo ID provided.")
 
-        # NOTE: Dummy Echo is used to spin up the cloud machine on app startup so subsequent requests are faster
-        if youtube_url == DUMMY_YOUTUBE_URL and echo_id == DUMMY_ECHO_ID:
-            logger.info("Skipping dummy Echo")
-            return
+        if not fileserver_url:
+            raise ValueError("No Fileserver URL provided.")
 
+        # Create a temporary file to store the downlaoded video
+        download_file = tempfile.NamedTemporaryFile(suffix=".mp4")
+        download_path = pathlib.Path(download_file.name)
+
+        # Download video
         YouTube(youtube_url).streams.filter(progressive=True, file_extension="mp4").order_by(
             "resolution"
-        ).asc().first().download(output_path=self.base_dir, filename=echo_id)
+        ).asc().first().download(output_path=download_path.parent.absolute(), filename=download_path.name)
 
-        self.drive.put(self._get_drive_filepath(echo_id))
-
-        os.remove(os.path.join(self.base_dir, echo_id))
-
-    def _get_drive_filepath(self, echo_id: str):
-        """Returns file path stored on the shared Drive."""
-        # NOTE: Drive throws `SameFileError` when using absolute path in `put()`, so we use relative path.
-        directory = self.base_dir.split(os.sep)[-1]
-
-        return os.path.join(directory, echo_id)
+        # Upload video to fileserver where it will be added to the shared Drive
+        requests.put(f"{fileserver_url}/upload/{echo_id}", files={"file": open(download_file.name, "rb")})
