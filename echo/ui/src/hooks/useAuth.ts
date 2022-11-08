8543de28448eece26ef521e89c0e09cee285a84b
Alec Merdler <alecmerdler@gmail.com>
2022-11-08 15:58:38 +0000

    Alert if Browser Blocks LocalStorage
    Some browser security settings will block access to
    'window.localStorage' from an <iframe>. We need local storage in
    order to save the user ID, so we need to inform the user to
    enable access in order for the app to work.

--- echo/ui/src/hooks/useAuth.ts
+++ echo/ui/src/hooks/useAuth.ts
@@ -3,6 +3,12 @@ import { useEffect } from "react";
 import { useQuery } from "react-query";
 import { echoClient } from "services/echoClient";
 
+const localStorageBlockedInfo =
+  "https://www.chromium.org/for-testers/bug-reporting-guidelines/uncaught-securityerror-failed-to-read-the-localstorage-property-from-window-access-is-denied-for-this-document/";
+const localStorageBlockedMessage = `
+  This app requires access to local storage in order to work properly. If you are using Chrome, you may need to disable the "Block third-party cookies" setting in order to use this app. For more information, see ${localStorageBlockedInfo}
+  `;
+
 const userIDKey = "echoUserID";
 
 type LoginResponse = {
@@ -10,6 +16,15 @@ type LoginResponse = {
 };
 
 export default function useAuth() {
+  // Some security settings block access to local storage from an <iframe>
+  try {
+    if (typeof window.localStorage === "undefined") {
+      throw new Error();
+    }
+  } catch (error) {
+    window.alert(localStorageBlockedMessage);
+  }
+
   const savedUserID = localStorage.getItem(userIDKey);
 
   const login = useQuery<LoginResponse>("login", () => echoClient.appApi.handleLoginApiLoginGet(), {
