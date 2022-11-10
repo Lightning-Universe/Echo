1719778108cf811288a155e931400973285acbc5
Alec Merdler <alecmerdler@gmail.com>
2022-11-10 15:31:38 +0000

    Reduce Sentry Noise (#120)
    Don't send 100% of exceptions to Sentry to avoid excess usage.

--- echo/ui/src/index.tsx
+++ echo/ui/src/index.tsx
@@ -16,7 +16,7 @@ if (process.env.NODE_ENV === "production") {
     // Set tracesSampleRate to 1.0 to capture 100%
     // of transactions for performance monitoring.
     // We recommend adjusting this value in production
-    tracesSampleRate: 1.0,
+    tracesSampleRate: 0.1,
   });
 }
 
