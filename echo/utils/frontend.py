e884f43270cf27c9ae15e4f668e7bb2cd34c256c
Alec Merdler <alecmerdler@gmail.com>
2022-11-22 14:51:33 +0000

    Fix Clone & Run Frontend Build
    The GitHub Action which deploys to lightning.ai/echo will build
    and upload the frontend artifacts to a GCP bucket. However, all
    'Clone & Run' instances also download their frontend builds from
    that bucket, which won't work because they require
    'ECHO_ROOT_PATH=/', whereas lightning.ai/echo uses
    'ECHO_ROOT_PATH=/echo'.
    
    The solution is to create a new bucket for the 'Clone & Run'
    instances to use, and upload a frontend build there which was
    built with 'ECHO_ROOT_PATH=/'.

--- echo/utils/frontend.py
+++ echo/utils/frontend.py
@@ -1,16 +1,23 @@
+import os
+import time
 from pathlib import Path
 from urllib.request import urlretrieve
 
-urls = [
-    "https://storage.googleapis.com/lightning-echo-prod/build/asset-manifest.json",
-    "https://storage.googleapis.com/lightning-echo-prod/build/favicon.svg",
-    "https://storage.googleapis.com/lightning-echo-prod/build/index.html",
-    "https://storage.googleapis.com/lightning-echo-prod/build/manifest.json",
-    "https://storage.googleapis.com/lightning-echo-prod/build/robots.txt",
+DEFAULT_BUCKET_URL = "https://storage.googleapis.com/lightning-echo-frontend"
+
+paths = [
+    "build/asset-manifest.json",
+    "build/favicon.svg",
+    "build/index.html",
+    "build/manifest.json",
+    "build/robots.txt",
 ]
 
 
 def download_frontend_build(destination: Path):
-    for url in urls:
-        file = Path(url.split("/")[-1])
-        urlretrieve(url, destination / file)
+    bucket_url = os.environ.get("ECHO_FRONTEND_BUCKET_URL", DEFAULT_BUCKET_URL)
+    cache_buster = round(time.time() * 1000)
+
+    for filepath in paths:
+        filename = Path(filepath.split("/")[-1])
+        urlretrieve(f"{bucket_url}/{filepath}?cacheBuster={cache_buster}", destination / filename)
