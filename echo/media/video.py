74a46493ec5f2f1757597f3554ba57334703a611
Alec Merdler <alecmerdler@gmail.com>
2022-11-26 19:56:33 +0000

    New Downloader Component using youtube-dl
    Implements a new 'Downloader' Work component which uses
    'youtube-dl' instead of 'PyTube'. This unlocks a plethora of
    internet sources which could benefit from transcription.
    
    This only implements the backend change; the frontend
    still only indicates support for YouTube videos.

--- echo/media/video.py
+++ echo/media/video.py
@@ -1,5 +1,9 @@
+from typing import List
+
 import ffmpeg
-from pytube import YouTube
+from youtube_dl import YoutubeDL
+from youtube_dl.extractor import gen_extractors
+from youtube_dl.extractor.common import InfoExtractor
 
 
 def contains_audio(video_file_path) -> bool:
@@ -11,13 +15,21 @@ def contains_audio(video_file_path) -> bool:
 
 def is_valid_youtube_url(youtube_url: str) -> bool:
     """Returns whether or not a given string is a valid YouTube URL."""
-    try:
-        YouTube(youtube_url)
-        return True
-    except Exception:
-        return False
+    extractors: List[InfoExtractor] = gen_extractors()
+    for e in extractors:
+        if e.suitable(youtube_url) and "youtube" in e.IE_NAME:
+            return True
+
+    return False
 
 
 def youtube_video_length(youtube_url: str):
     """Returns the length of a YouTube video."""
-    return YouTube(youtube_url).length
+    ydl_opts = {
+        "format": "worst[ext=mp4]",
+    }
+
+    with YoutubeDL(ydl_opts) as ydl:
+        info_dict = ydl.extract_info(youtube_url, download=False)
+
+        return info_dict["duration"]
