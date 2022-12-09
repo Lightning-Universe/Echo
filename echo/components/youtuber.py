0ddfab94719e3eb7aa05ae266362ca0ac601502c
Alec Merdler <alecmerdler@gmail.com>
2022-12-09 17:02:38 +0000

    Switch Back to YouTuber Component
    The 'youtube-dl' component is receiving too many HTTP 403
    responses in production. Switching back to using the old
    YouTuber component.

--- echo/components/youtuber.py
+++ echo/components/youtuber.py
@@ -24,6 +24,7 @@ class YouTuber(LightningWork):
             parallel=True,
             cloud_compute=CloudCompute(cloud_compute),
             cloud_build_config=BuildConfig(requirements=["pytube"]),
+            raise_exception=False,
         )
 
         init_sentry()
